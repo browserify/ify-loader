c47745a9768dcc60e151f3ae248a2b5c4057b380
Ricky Reusser <rsreusser@gmail.com>
2017-07-25 08:00:39 -0700

    Add failing fixture for glsl

---
+++ fixtures/glsl/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js/,
+        use: path.join(__dirname, '../../')
+      }
+    ]
+  }
+}
